7a312a9d0d5ff51ccb2403b4000bc15f49f29ba0
chanan-duy <chanan.duy@proton.me>
2025-10-24 20:51:29 +0500

    `Wpf.Fractals` fix drawing logic

--- Wpf.Fractals/MainWindow.xaml.cs
+++ Wpf.Fractals/MainWindow.xaml.cs
@@ -16,17 +16,22 @@ public partial class MainWindow
         FractalsCanvas.Children.Clear();
 
         var center = new Point(FractalsCanvas.ActualWidth / 2, FractalsCanvas.ActualHeight / 2);
-        var sideLength = Math.Min(FractalsCanvas.ActualWidth, FractalsCanvas.ActualHeight) * 0.4;
-        var shrinkSideBy = 0.5;
-        var maxDepth = 2;
-        var color = Brushes.Gold;
+        var sideLength = Math.Min(FractalsCanvas.ActualWidth, FractalsCanvas.ActualHeight) * 0.6;
+        var shrinkSideBy = 0.6;
+        var maxDepth = 6;
+        var colors = new[] { Brushes.Gold, Brushes.Cyan, Brushes.Magenta, Brushes.Bisque, Brushes.Chartreuse };
 
-        DrawRecursiveTriangle(center, sideLength, 0, 1, color, shrinkSideBy, 0, maxDepth);
+        DrawRecursiveTriangle(center, sideLength, 0, 1, colors, shrinkSideBy, 0, maxDepth);
     }
 
-    private void DrawRecursiveTriangle(Point center, double sideLength, double degreesRotate, int thickness, SolidColorBrush color,
+    private void DrawRecursiveTriangle(Point center, double sideLength, double degreesRotate, int thickness, SolidColorBrush[] colors,
         double shrinkSideBy, int iteration, int maxDepth)
     {
+        if (iteration > maxDepth)
+        {
+            return;
+        }
+
         var triangleHeight = sideLength * Math.Sqrt(3) / 2;
 
         var top = center with { Y = center.Y - 2.0 / 3.0 * triangleHeight };
@@ -40,61 +45,67 @@ public partial class MainWindow
             bottomRight = Rotate(bottomRight, center, degreesRotate);
         }
 
-        var linesToDraw = new[] { (top, bottomLeft), (top, bottomRight), (bottomLeft, bottomRight) };
-        foreach (var (start, end) in linesToDraw)
-        {
-            PlotLine(start, end, thickness, color);
-        }
+        PlotLine(top, bottomLeft, thickness, colors[iteration % colors.Length]);
+        PlotLine(top, bottomRight, thickness, colors[iteration % colors.Length]);
+        PlotLine(bottomLeft, bottomRight, thickness, colors[iteration % colors.Length]);
 
         if (iteration >= maxDepth)
         {
             return;
         }
 
-        var recursiveLines = new[] { (start: top, end: bottomLeft), (start: top, end: bottomRight), (start: bottomLeft, end: bottomRight) };
-        var lineNumber = 0;
+        var recursiveLines = new[] { (top, bottomLeft), (top, bottomRight), (bottomLeft, bottomRight) };
 
+        var lineNumber = 0;
         foreach (var (startPoint, endPoint) in recursiveLines)
         {
             lineNumber++;
 
-            if (iteration < 1 || lineNumber < 3)
+            if (!(iteration < 1 || lineNumber < 3))
             {
-                var newSideLength = sideLength * shrinkSideBy;
-                var newRotation = degreesRotate;
-
-                switch (lineNumber)
-                {
-                    case 1:
-                        newRotation += 60;
-                        break;
-                    case 2:
-                        newRotation -= 60;
-                        break;
-                    case 3:
-                        newRotation += 180;
-                        break;
-                }
-
-                var newCenter = GetNewOuterCenter(center, startPoint, endPoint, newSideLength);
-
-                DrawRecursiveTriangle(newCenter, newSideLength, newRotation, thickness, color, shrinkSideBy, iteration + 1, maxDepth);
+                continue;
             }
+
+            var newSideLength = sideLength * shrinkSideBy;
+            var newRotation = degreesRotate;
+
+            switch (lineNumber)
+            {
+                case 1:
+                    newRotation += -60;
+                    break;
+                case 2:
+                    newRotation += 60;
+                    break;
+                case 3:
+                    newRotation += 180;
+                    break;
+            }
+
+            var newCenter = GetNewOuterCenter(center, startPoint, endPoint, newSideLength);
+
+            DrawRecursiveTriangle(newCenter, newSideLength, newRotation, thickness, colors, shrinkSideBy, iteration + 1, maxDepth);
         }
     }
 
     private static Point GetNewOuterCenter(Point parentCenter, Point lineStart, Point lineEnd, double newSideLength)
     {
         var centerOfLine = new Point((lineStart.X + lineEnd.X) / 2, (lineStart.Y + lineEnd.Y) / 2);
-
         var newTriangleHeight = newSideLength * Math.Sqrt(3) / 2;
 
-        var outwardVector = centerOfLine - parentCenter;
-        outwardVector.Normalize();
+        var lineVector = lineEnd - lineStart;
+        var normalVector = new Vector(-lineVector.Y, lineVector.X);
+
+        var vectorToParentCenter = parentCenter - centerOfLine;
+        if (Vector.Multiply(normalVector, vectorToParentCenter) > 0)
+        {
+            normalVector *= -1;
+        }
+
+        normalVector.Normalize();
 
         var offsetDistance = newTriangleHeight / 3.0;
-
-        return centerOfLine + outwardVector * offsetDistance;
+        return centerOfLine + normalVector * offsetDistance;
     }
 
     private static Point Rotate(Point pointToRotate, Point centerPoint, double degrees)
@@ -102,13 +113,9 @@ public partial class MainWindow
         var radians = degrees * (Math.PI / 180);
         var cos = Math.Cos(radians);
         var sin = Math.Sin(radians);
-
-        var x = pointToRotate.X - centerPoint.X;
-        var y = pointToRotate.Y - centerPoint.Y;
-
-        var newX = x * cos - y * sin;
-        var newY = y * cos + x * sin;
-
+        var vector = pointToRotate - centerPoint;
+        var newX = vector.X * cos - vector.Y * sin;
+        var newY = vector.X * sin + vector.Y * cos;
         return new Point(newX + centerPoint.X, newY + centerPoint.Y);
     }
 
